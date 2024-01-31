7a726572b58cc4570d989b2591fd06b646851a68
vijay010498 <svijayr007@gmail.com>
2024-01-30 23:59:23 -0500

    commented out the upload files code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,11 +10,10 @@ import { CurrentUserInterceptor } from '../common/interceptors/current-user.inte
 import { Serialize } from '../common/interceptors/serialize.interceptor';
 import { UserDto } from '../common/dtos/user.dto';
 import { UserService } from './user.service';
-import { AccessTokenGuard } from '../common/guards/accessToken.guard';
-import { IsBlockedGuard } from '../common/guards/isBlocked.guard';
-import { TokenBlacklistGuard } from '../common/guards/tokenBlacklist.guard';
-import { CurrentUser } from '../common/decorators/current-user.decorator';
-import { FileFieldsInterceptor } from '@nestjs/platform-express';
+// import { AccessTokenGuard } from '../common/guards/accessToken.guard';
+// import { IsBlockedGuard } from '../common/guards/isBlocked.guard';
+// import { TokenBlacklistGuard } from '../common/guards/tokenBlacklist.guard';
+// import { FileFieldsInterceptor } from '@nestjs/platform-express';
 
 @Controller('user')
 @UseInterceptors(CurrentUserInterceptor)
@@ -22,27 +21,27 @@ import { FileFieldsInterceptor } from '@nestjs/platform-express';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(AccessTokenGuard, IsBlockedGuard, TokenBlacklistGuard)
-  @Post()
-  @UseInterceptors(
-    FileFieldsInterceptor([
-      {
-        name: 'selfi',
-        maxCount: 1,
-      },
-      {
-        name: 'photoId',
-        maxCount: 1,
-      },
-    ]),
-  )
-  verifyUser(
-    @UploadedFiles()
-    images: {
-      selfi?: Express.Multer.File[];
-      photoId?: Express.Multer.File[];
-    },
-  ) {
-    return images;
-  }
+  // @UseGuards(AccessTokenGuard, IsBlockedGuard, TokenBlacklistGuard)
+  // @Post()
+  // @UseInterceptors(
+  //   FileFieldsInterceptor([
+  //     {
+  //       name: 'selfi',
+  //       maxCount: 1,
+  //     },
+  //     {
+  //       name: 'photoId',
+  //       maxCount: 1,
+  //     },
+  //   ]),
+  // )
+  // verifyUser(
+  //   @UploadedFiles()
+  //   images: {
+  //     selfi?: Express.Multer.File[];
+  //     photoId?: Express.Multer.File[];
+  //   },
+  // ) {
+  //   return images;
+  // }
 }
